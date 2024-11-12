6149f69640c25037507555661aa2e7a6a2c1bb72
Abcd789JK <185043110+Abcd789JK@users.noreply.github.com>
2024-11-12 14:56:52 +0800

    Update 2024-11-12 14:56:52

--- Script/Beta/mihomo/update.sh
+++ Script/Beta/mihomo/update.sh
@@ -74,12 +74,12 @@ download_mihomo() {
     local filename
     get_schema
     download_version
-    [[ "$arch" == 'amd64' ]] && filename="mihomo-linux-${arch}-go120-${version}.gz" ||
+    [[ "$arch" == 'amd64' ]] && filename="mihomo-linux-${arch}-compatible-go120-${version}.gz" ||
     filename="mihomo-linux-${arch}-${version}.gz"
     local download_url=$(get_url "https://github.com/MetaCubeX/mihomo/releases/download/Prerelease-Alpha/${filename}")
     wget -t 3 -T 30 "${download_url}" -O "${filename}" || { echo -e "${red}mihomo 下载失败，可能是网络问题，建议重新运行本脚本重试下载${reset}"; exit 1; }
     gunzip "$filename" || { echo -e "${red}mihomo 解压失败${reset}"; exit 1; }
-    mv "mihomo-linux-${arch}-go120-${version}" mihomo 2>/dev/null || mv "mihomo-linux-${arch}-${version}" mihomo || { echo -e "${red}找不到解压后的文件${reset}"; exit 1; }
+    mv "mihomo-linux-${arch}-compatible-go120-${version}" mihomo 2>/dev/null || mv "mihomo-linux-${arch}-${version}" mihomo || { echo -e "${red}找不到解压后的文件${reset}"; exit 1; }
     chmod +x mihomo
     echo "$version" > "$version_file"
 }
